ef54f03488e5e8ab266c8f352546755817a74fb8
sevbad <sbadriddinova@usf.edu>
2026-04-07 10:57:43 -0400

    fix: vite build + tsconfig + imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./LandingPage";
 import DashboardPage from "./pages/DashboardPage";
-import {ProtectedLayout} from "./layouts/ProtectedLayout";
-import {PublicLayout} from "./layouts/PublicLayout";
+import ProtectedLayout from "./layouts/ProtectedLayout";
+import PublicLayout from "./layouts/PublicLayout";
 
 export default function App() {
     return {}
